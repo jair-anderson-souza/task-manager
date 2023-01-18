307ad3d25b84b3bcc69166cb05b5e762dbfc330b
jair-anderson-souza <jair.silva@quintoandar.com.br>
2023-01-17 23:40:28 -0300

    init

---
+++ lib/minha_primeira_tela.dart
@@ -0,0 +1,19 @@
+// import 'package:flutter/material.dart';
+
+// class ScaffoldWidget extends StatelessWidget {
+//   const ScaffoldWidget({super.key});
+
+//   @override
+//   Widget build(BuildContext build) {
+//     return MaterialApp(
+//       theme: ThemeData(
+//         primarySwatch: 
+//       ),
+//         title: 'Minha primeira tela',
+//         color: Colors.amber,
+//         home: Scaffold(
+//           appBar: AppBar(title: const Text('My first Appbar')),
+//           body: Container(color: Colors.amber),
+//         ));
+//   }
+// }
