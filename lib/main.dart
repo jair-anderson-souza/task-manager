f9cbd6c106b17fc0b55ac383d36694d5356fd7cb
jair-anderson-souza <jair.silva@quintoandar.com.br>
2023-02-15 17:48:37 -0300

    create screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:starter_name/form_screen.dart';
 import 'package:starter_name/task.dart';
 
 void main() {
-  runApp(const ScaffoldWidget());
+  // runApp(const ScaffoldWidget());
+  runApp(ScaffoldWidget());
 }
 
 class ScaffoldWidget extends StatefulWidget {
@@ -13,8 +15,6 @@ class ScaffoldWidget extends StatefulWidget {
 }
 
 class _ScaffoldWidgetState extends State<ScaffoldWidget> {
-  bool opacity = true;
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -26,26 +26,24 @@ class _ScaffoldWidgetState extends State<ScaffoldWidget> {
               leading: Container(),
               title: const Text('Tarefas'),
             ),
-            body: AnimatedOpacity(
-              opacity: opacity ? 1 : 0,
-              duration: const Duration(milliseconds: 500),
-              child: ListView(
-                children: [
-                  Task('Aprender Flutter', 'assets/images/flutter.png', 3),
-                  Task('Andar de Bike', 'assets/images/bike.jpg', 2),
-                  Task('Meditar', 'assets/images/meditar.jpeg', 5),
-                  Task('Ler', 'assets/images/ler.jpeg', 2),
-                  Task('Jogar', 'assets/images/jogar.jpeg', 1),
-                  const SizedBox(height: 50)
-                ],
-              ),
+            body: ListView(
+              children: [
+                Task('Aprender Flutter', 'assets/images/flutter.png', 3),
+                Task('Andar de Bike', 'assets/images/bike.jpg', 2),
+                Task('Meditar', 'assets/images/meditar.jpeg', 5),
+                Task('Ler', 'assets/images/ler.jpg', 2),
+                Task('Jogar', 'assets/images/jogar.jpg', 1),
+                const SizedBox(height: 50)
+              ],
             ),
             floatingActionButton: FloatingActionButton(
-                onPressed: () {
-                  setState(() {
-                    opacity = !opacity;
-                  });
-                },
-                child: const Icon(Icons.remove_red_eye))));
+              onPressed: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const FormScreen(),
+                ),
+              ),
+              child: const Icon(Icons.add),
+            )));
   }
 }
